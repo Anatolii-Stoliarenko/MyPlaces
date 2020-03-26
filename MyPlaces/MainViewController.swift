f31a23b1a1db305b384a07113a4857760e6d6f7c
Anatolii Stoliarenko <nokiasto@gmail.com>
2020-03-27 00:19:40 +0100

    Launch Screen Row Hight

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -28,10 +28,16 @@ class MainViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
-  
-
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2 //заркугляем imageView
+        cell.imageView?.clipsToBounds = true //обрезаем изображение
 
         return cell
+    }
+    
+    //MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 
 
